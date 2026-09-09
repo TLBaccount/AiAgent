3192b9c36183292e354b6bd732c95637b662ee0f
TLBaccount <feth.eddine.taleb88@gmail.com>
2026-09-09 17:53:01 +0100

    Update tts.js

--- api/tts.js
+++ api/tts.js
@@ -13,9 +13,9 @@ export default async function handler(req, res) {
     // Français : f3f1d5e0-6a2b-4b1d-9b2c-1f3a5d7e9b01
     // Anglais : 7f2b3a5c-8e1d-4f6a-9c3b-2e5d7a9c1f03
     // Arabe : 6a4e3b2d-8c1f-4a5e-9d7b-3f2a1c4e5d67
-    let voiceId = "79d0bd3e4e5444b18f7b6d89b5927bf1"; // Français par défaut
+    let voiceId = "fe118d40f7e042dd86143e2938f0cc2e"; // Français par défaut
     if (lang === 'en') voiceId = "79d0bd3e4e5444b18f7b6d89b5927bf1";
-    if (lang === 'ar') voiceId = "79d0bd3e4e5444b18f7b6d89b5927bf1";
+    if (lang === 'ar') voiceId = "22a57197aa594615b96575b9cb021419";
 
     try {
         // 1. Générer l'audio avec Fish Audio
