c4a7b376099a7e67d4ff9c4665494f2c133aadd7
TLBaccount <feth.eddine.taleb88@gmail.com>
2026-09-16 10:51:14 +0100

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -85,7 +85,8 @@ export default async function handler(req, res) {
             body: JSON.stringify({ 
                 message: userText, 
                 history: history,
-                forcedLang: detectedLang
+                forcedLang: detectedLang,
+                channel: "telegram"
             })
         });
 
